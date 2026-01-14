87068217c18907ba003890b664ea70ea4e93553f
eXTReMe <mitya.cotlov2017@yandex.ru>
2026-01-14 18:07:16 +0400

    Тест1

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,12 @@
 namespace TestGit
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
     }
+
+    private void button1_Click(object sender, EventArgs e) {
+
+    }
+  }
 }
